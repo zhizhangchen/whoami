dbc8ac44423159c826706fb2f1b3830c5cb4ada5
John Chen <john.chen@paysafe.com>
2017-12-18 16:56:05 -0500

    Remove radio box from RemoteViews and fix preference problem

--- app/src/androidTest/java/ca/blogspot/johnchenprogramming/whoami/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ca/blogspot/johnchenprogramming/whoami/ExampleInstrumentedTest.kt
@@ -1,8 +1,6 @@
 package ca.blogspot.johnchenprogramming.whoami
 
-import android.content.Context
 import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,19 +9,11 @@ import org.junit.Assert.*
 import android.support.test.uiautomator.UiDevice
 import android.support.test.uiautomator.By
 import android.support.test.uiautomator.Until
-import java.io.File
 
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
-        clearPreferences()
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         device.openNotification();
         device.wait(Until.hasObject(By.text(NotificationScheduler.TITLE)), 10000)
@@ -33,14 +23,4 @@ class ExampleInstrumentedTest {
         assertEquals(NotificationScheduler.SUB_TITLE, subtitle.getText())
     }
 
-    private fun clearPreferences() {
-        val root = InstrumentationRegistry.getTargetContext().filesDir.parentFile
-        val sharedPreferencesFileNames = File(root, "shared_prefs").list()
-        for (fileName in sharedPreferencesFileNames) {
-            InstrumentationRegistry.getTargetContext().getSharedPreferences(
-                    fileName.replace(".xml", ""),
-                    Context.MODE_PRIVATE
-            ).edit().clear().apply()
-        }
-    }
 }
