6f7ad655b4dc0a6d826ae5d8f68ff5e08ed61cd6
John Chen <john.chen@paysafe.com>
2017-12-23 22:01:07 -0500

    Increase text size of button

--- app/src/androidTest/java/ca/blogspot/johnchenprogramming/whoami/WelcomeActivityUITest.kt
+++ app/src/androidTest/java/ca/blogspot/johnchenprogramming/whoami/WelcomeActivityUITest.kt
@@ -4,6 +4,7 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
+import ca.blogspot.johnchenprogramming.whoami.ViewAssertions.hasTextSize
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -22,5 +23,10 @@ class WelcomeActivityUITest {
         sleep(1000)
         onView(withText(R.string.button)).perform(click())
         assertTrue(activityRule.activity.isFinishing);
+    }
+
+    @Test
+    fun buttonTextSize_is20sp() {
+        onView(withText(R.string.button)).check(hasTextSize(R.dimen.button_size));
     }
 }
