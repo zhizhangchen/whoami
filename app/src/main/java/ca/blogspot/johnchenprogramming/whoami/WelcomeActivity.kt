c565c201c4c612929773449bf18a46feb863a097
John Chen <john.chen@paysafe.com>
2017-12-29 11:37:49 -0500

    Check SDK version for system UI visibility

--- app/src/main/java/ca/blogspot/johnchenprogramming/whoami/WelcomeActivity.kt
+++ app/src/main/java/ca/blogspot/johnchenprogramming/whoami/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package ca.blogspot.johnchenprogramming.whoami
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -20,11 +21,18 @@ class WelcomeActivity : AppCompatActivity() {
         // at compile-time and do nothing on earlier devices.
         fullscreen_content.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            fullscreen_content.systemUiVisibility = fullscreen_content.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            fullscreen_content.systemUiVisibility = fullscreen_content.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
     }
     private val mShowPart2Runnable = Runnable {
         // Delayed display of UI elements
@@ -82,9 +90,11 @@ class WelcomeActivity : AppCompatActivity() {
 
     private fun show() {
         // Show the system bar
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            fullscreen_content.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
         mVisible = true
 
         // Schedule a runnable to display UI elements after a delay
